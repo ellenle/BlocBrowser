cc7a96e6348c381baaab084ab6752becd4297d67
Ellen <ellenleis@gmail.com>
2016-01-03 22:32:24 -0800

    checkpoint 23 setup

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Ellen Thuy Le on 11/28/15.
+//  Copyright (c) 2015 Ellen Thuy Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
